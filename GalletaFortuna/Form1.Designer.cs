62f0525189d831baf5aa56f94191acc368453e32
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-02-07 23:01:55 -0600

    Se agrega el Proyecto

---
+++ GalletaFortuna/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GalletaFortuna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Mensaje
+            // 
+            this.Mensaje.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Mensaje.Location = new System.Drawing.Point(72, 110);
+            this.Mensaje.Name = "Mensaje";
+            this.Mensaje.Size = new System.Drawing.Size(196, 50);
+            this.Mensaje.TabIndex = 1;
+            this.Mensaje.Text = "Mensaje Aleatorio";
+            this.Mensaje.UseVisualStyleBackColor = false;
+            this.Mensaje.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 259);
+            this.Controls.Add(this.Mensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Mensaje;
+    }
+}
+
---
+++ GalletaFortuna/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GalletaFortuna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string[] mensajesalea =
+        {
+            "Buena suerte en el semestre",
+            "Tendras un buen año",
+            "Dios te dara muchas alegrias este nuevo año",
+            "Pasaras un buen cumpleaños",
+            "Haras nuevas y buenas amistades",
+            "Que cada día esté lleno de bendiciones y buena suerte.",
+            "Presentacion programa"
+        };
+
+        private Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int imensaje = random.Next(mensajesalea.Length);
+            MessageBox.Show(mensajesalea[imensaje], "Mensaje Random");
+        }
+    }
+}
